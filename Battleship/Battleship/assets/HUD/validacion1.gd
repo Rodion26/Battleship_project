230a8b67d03d0a446ce6e5139b52e72c9b9bb53a
Michael Quilachamin <90359984+Rodion26@users.noreply.github.com>
2022-05-31 20:07:43 -0500

    Se agrega el resto del trabajo.
    Actualización de lo que tenemos hasta ahora.

---
+++ Battleship/Battleship/assets/HUD/validacion1.gd
@@ -0,0 +1,18 @@
+extends LineEdit
+var filas1 : String
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in range(filas1):
+	  if filas1(i)>="0"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
