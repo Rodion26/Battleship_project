43aa2d48a3fc85e03e20c97eaa6ac7eb367983c9
NicoJRM <106696406+NicoJRM@users.noreply.github.com>
2022-06-01 22:41:24 -0500

    Add files via upload
    Diseño filas y columnas

---
+++ Godot/assets/Menu/menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var fistInput=0
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://assets/level/level.tscn")
+
+
+func _on_Exit_pressed(): #Función en donde se asigna el método ded salida del boton exit
+	get_tree().quit()
